c03be7f2e58e452974112943087185e58c1c42c4
AbhayDeep04 <117099810+AbhayDeep04@users.noreply.github.com>
2023-10-23 19:51:04 -0400

    Update TemperatureChart.jsx

--- src/TemperatureChart.jsx
+++ src/TemperatureChart.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+
+function TemperatureChart({ weatherData }) {
+
+  const data = weatherData.map(item => ({
+    city_name: item.city_name,
+    temperature: item.temp,
+  }));
+
+  return (
+    <div>
+      <h2>Temperature Chart</h2>
+      <BarChart width={600} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="city_name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="temperature" fill="#8884d8" />
+      </BarChart>
+    </div>
+  );
+}
+
+export default TemperatureChart;
 

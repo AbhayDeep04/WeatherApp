919439c59836f3b3de4272afe898788813b2c637
AbhayDeep04 <117099810+AbhayDeep04@users.noreply.github.com>
2023-10-23 19:49:25 -0400

    Update WeatherDetail.jsx

--- src/WeatherDetail.jsx
+++ src/WeatherDetail.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function WeatherDetail({ weatherData }) {
+  const { cityName } = useParams();
+
+  const selectedWeatherData = weatherData.find(data => data.city_name === cityName);
+
+  if (!selectedWeatherData) {
+    return <div>Weather data not found for this city.</div>;
+  }
+
+  return (
+    <div>
+      <h2>Weather Detail for {selectedWeatherData.city_name}</h2>
+      <p>Temperature: {selectedWeatherData.temp}°C</p>
+      <p>Condition: {selectedWeatherData.weather.description}</p>
+    </div>
+  );
+}
+
+export default WeatherDetail;
 
